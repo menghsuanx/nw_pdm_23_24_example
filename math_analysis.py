31a29c8517be036999d8c3b5cce1f831b6738b05
LeClaire <leclan1@nationwide.com>
2023-09-27 12:50:53 -0400

    first iteration of math_tools and math_analysis

---
+++ math_analysis.py
@@ -0,0 +1,47 @@
+"""
+
+Does some calculations using math_tools.
+
+This script provides examples of how useful and cool the math_tools module is, 
+calculating several quantities of interest and printing them.
+
+AUTHOR: leclan1
+DATE: 2023-09-28
+
+Classes:
+    
+    n/a
+    
+Functions:
+    
+    n/a
+
+Misc variables:
+    
+    n/a
+    
+"""
+
+import math_tools
+
+# What is 2 plus 2?
+print("What is 2 plus 2?")
+print(math_tools.add(2, 2))
+
+# What is 3 to the 3 power?
+print("What is 3 to the 3 power?")
+print(math_tools.exponentiate(3, 3))
+
+# What is the area of a circle with radius 5?
+print("What is the area of a circle with radius 5?")
+print(math_tools.multiply(math_tools.pi
+                          , math_tools.exponentiate(5, 2)))
+
+# How much will $1 grow to if compounded continuously for three years at an
+#   annual rate of 6%?
+print("How much will $1 grow to if compounded continuously for three years " \
+      + "at an annual rate of 6%?")
+print(math_tools.multiply(1
+                          , math_tools.exponentiate(math_tools.e
+                                                    , math_tools.multiply(3
+                                                                          , .06))))
---
+++ math_tools.py
@@ -0,0 +1,43 @@
+"""
+
+Provides useful math functions and constants.
+
+This script provides functions for mathematical computations, as well as
+commonly used mathematical constants for reference in other scripts.
+
+AUTHOR: leclan1
+DATE: 2023-09-28
+
+Classes:
+    
+    n/a
+    
+Functions:
+    
+    add(a, b) -> float
+    exponentiate(base, power) -> float
+
+Misc variables:
+    
+    pi: the ratio of a circle's circumference to its diameter
+    e: the base of the natural logarithm
+    
+"""
+
+def add(a, b):
+    """Returns the sum of a and b"""
+    return(a + b)
+
+
+def multiply(a, b):
+    """Returns the sum of a and b"""
+    return(a * b)
+
+
+def exponentiate(base, power):
+    """Returns base raised to the power power"""
+    return(base ** power)
+
+
+pi = 3.142
+e = 2.718
